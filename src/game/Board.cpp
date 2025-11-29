b8ea320902f9fd11266e88ef6c80c3a93308874f
puro <purohitkrishnar@gmail.com>
2025-11-29 01:49:33 +0000

    Adding all pieces

--- src/game/Board.cpp
+++ src/game/Board.cpp
@@ -38,6 +38,107 @@ void Board::initialize_pieces() {
 			std::make_pair(6, i),
 			static_cast<char>('A' + i) + std::to_string(7));
 	}
+
+	// rooks
+	board_[0][0] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::ROOK,
+		std::make_pair(0, 0),
+		"A1");
+
+	board_[0][7] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::ROOK,
+		std::make_pair(0, 7),
+		"H1");
+
+	board_[7][0] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::ROOK,
+		std::make_pair(7, 0),
+		"A8");
+
+	board_[7][7] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::ROOK,
+		std::make_pair(7, 7),
+		"H8");
+
+	// knights
+	board_[0][1] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::KNIGHT,
+		std::make_pair(0, 1),
+		"B1");
+
+	board_[0][6] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::KNIGHT,
+		std::make_pair(0, 6),
+		"G1");
+
+	board_[7][1] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::KNIGHT,
+		std::make_pair(7, 1),
+		"B8");
+
+	board_[7][6] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::KNIGHT,
+		std::make_pair(7, 6),
+		"G8");
+
+	// bishops
+	board_[0][2] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::BISHOP,
+		std::make_pair(0, 2),
+		"C1");
+
+	board_[0][5] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::BISHOP,
+		std::make_pair(0, 5),
+		"F1");
+
+	board_[7][2] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::BISHOP,
+		std::make_pair(7, 2),
+		"C8");
+
+	board_[7][5] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::BISHOP,
+		std::make_pair(7, 5),
+		"F8");
+
+	// queens
+	board_[0][3] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::QUEEN,
+		std::make_pair(0, 3),
+		"D1");
+
+	board_[7][3] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::QUEEN,
+		std::make_pair(7, 3),
+		"D8");
+
+	// kings
+	board_[0][4] = std::make_shared<Piece>(
+		Side::WHITE,
+		PieceType::KING,
+		std::make_pair(0, 4),
+		"E1");
+
+	board_[7][4] = std::make_shared<Piece>(
+		Side::BLACK,
+		PieceType::KING,
+		std::make_pair(7, 4),
+		"E8");
 }
 
 
